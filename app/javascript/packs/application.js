1bac482bc88c7b48d1e93c631af9631b76a1b29f
Lilith <li6253462@gmail.com>
2022-02-12 13:26:47 +0800

    add select datetime function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require bootstrap-datepicker
+
+$(document).ready(function(){
+  $('.datepicker').datepicker({
+    format: 'dd/mm/yyyy'
+  });
+});
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
